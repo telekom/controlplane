3efaee691bccf1170ab7d96586c78f1547bb62f0
Ron Gummich <ron.gummich@telekom.de>
2025-05-23 14:10:01 +0200

    ci: added install script

---
+++ .releaserc.mjs
@@ -0,0 +1,33 @@
+// Copyright 2025 Deutsche Telekom IT GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// @ts-check
+
+/** @type {import('semantic-release').GlobalConfig} */
+export default {
+    preset: 'angular',
+    tagFormat: 'v${version}',
+    repositoryUrl: 'https://github.com/telekom/controlplane.git',
+    branches: [
+        'master',
+        'main',
+        'next',
+        'next-major',
+    ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        ['@semantic-release/exec', {
+            prepareCmd: `bash ./update_install.sh "\${nextRelease.gitTag}"`,
+            publishCmd: `echo "\${nextRelease.notes}" > /tmp/release-notes.md`,
+        }],
+        ['@semantic-release/git', {
+            assets: [
+                'CHANGELOG.md',
+                'install/kustomization.yaml',
+            ],
+        }],
+    ],
+};
---
+++ update_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright 2025 Deutsche Telekom IT GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+KUSTOMIZATION_FILE="install/kustomization.yaml"
+NEXT_VERSION="$1"
+if [ -z "$NEXT_VERSION" ]; then
+  echo "Usage: $0 <next-version>"
+  exit 1
+fi
+
+sed -i "s/ref=[^ ]*/ref=${NEXT_VERSION}/" "$KUSTOMIZATION_FILE"
+sed -i "s/newTag: [^ ]*/newTag: ${NEXT_VERSION}/" "$KUSTOMIZATION_FILE"
