a0ca9e6e7592a0d96b12e86fef4e8337a2f1a403
stefan-ctrl <stefan-ctrl@users.noreply.github.com>
2025-07-16 13:36:35 +0200

    refactor(common): only do envs in common

--- common/pkg/config/config.go
+++ common/pkg/config/config.go
@@ -5,17 +5,13 @@
 package config
 
 import (
-	"errors"
-	"flag"
 	"time"
 
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
 // Configuration key constants
 const (
-	configKeyFile                = "config"
 	configKeyRequeueAfterOnError = "requeue-after-on-error"
 	configKeyRequeueAfter        = "requeue-after"
 	configKeyDefaultNamespace    = "default-namespace"
@@ -27,39 +23,34 @@ const (
 	configKeyMaxConcurrentRec    = "max-concurrent-reconciles"
 )
 
+const (
+	FinalizerSuffix = "finalizer"
+)
+
 // exposed configuration variables
 var (
 	// RequeueAfterOnError is the time to wait before retrying a failed operation.
 	// This applies for all controller errors.
 	RequeueAfterOnError = 1 * time.Second
 	// RequeueAfter is the time to wait before retrying a successful operation.
-	RequeueAfter       = 30 * time.Minute
-	DefaultNamespace   = "default"
-	DefaultEnvironment = "default"
-	LabelKeyPrefix     = "cp.ei.telekom.de"
-	FinalizerSuffix    = "finalizer"
-	FinalizerName      = LabelKeyPrefix + "/" + FinalizerSuffix
+	RequeueAfter = 30 * time.Minute
 	// JitterFactor is the factor to apply to the backoff duration.
 	JitterFactor = 0.7
 	// MaxBackoff is the maximum backoff duration.
 	MaxBackoff = 5 * time.Minute
 	// MaxConcurrentReconciles is the maximum number of concurrent reconciles.
 	MaxConcurrentReconciles = 10
+
+	DefaultNamespace   = "default"
+	DefaultEnvironment = "default"
+	LabelKeyPrefix     = "cp.ei.telekom.de"
+	FinalizerName      = LabelKeyPrefix + "/" + FinalizerSuffix
 )
 
 func init() {
 	registerDefaults()
-
-	if err := registerEnvs(); err != nil {
-		panic("failed to bind environment variables: " + err.Error())
-	}
-	registerFlag()
-
-	// go run ./cmd/main.go --metrics-bind-address=:8080 --jitter-factor=0.9
-	// results in "flag provided but not defined: -metrics-bind-address" (so all in main.go are not registered here)
-	// if err := Parse(); err != nil {
-	// 	panic("failed to parse configuration: " + err.Error())
-	// }
+	registerEnvsOrDie()
+	Parse()
 }
 
 func registerDefaults() {
@@ -68,103 +59,43 @@ func registerDefaults() {
 	viper.SetDefault(configKeyDefaultNamespace, DefaultNamespace)
 	viper.SetDefault(configKeyDefaultEnvironment, DefaultEnvironment)
 	viper.SetDefault(configKeyLabelKeyPrefix, LabelKeyPrefix)
-	viper.SetDefault(configKeyFinalizerSuffix, FinalizerSuffix)
 	viper.SetDefault(configKeyJitterFactor, JitterFactor)
 	viper.SetDefault(configKeyMaxBackoff, MaxBackoff)
 	viper.SetDefault(configKeyMaxConcurrentRec, MaxConcurrentReconciles)
 }
 
-func registerFlag() {
-	// Config file flag
-	flag.String(configKeyFile, "", "Path to the configuration file")
+func registerEnvsOrDie() {
+	// Explicitly bind environment variables to configuration keys
 
-	// Add flags for all configuration parameters
-	flag.Duration(configKeyRequeueAfterOnError, RequeueAfterOnError, "Time to wait before retrying a failed operation")
-	flag.Duration(configKeyRequeueAfter, RequeueAfter, "Time to wait before retrying a successful operation")
-	flag.String(configKeyDefaultNamespace, DefaultNamespace, "Default namespace")
-	flag.String(configKeyDefaultEnvironment, DefaultEnvironment, "Default environment")
-	flag.String(configKeyLabelKeyPrefix, LabelKeyPrefix, "Label key prefix")
-	flag.String(configKeyFinalizerSuffix, FinalizerSuffix, "Finalizer name suffix")
-	flag.Float64(configKeyJitterFactor, JitterFactor, "Factor to apply to the backoff duration")
-	flag.Duration(configKeyMaxBackoff, MaxBackoff, "Maximum backoff duration")
-	flag.Int(configKeyMaxConcurrentRec, MaxConcurrentReconciles, "Maximum number of concurrent reconciles")
+	envKeyMap := map[string]string{
+		configKeyRequeueAfterOnError: "REQUEUE_AFTER_ON_ERROR",
+		configKeyRequeueAfter:        "REQUEUE_AFTER",
+		configKeyDefaultNamespace:    "DEFAULT_NAMESPACE",
+		configKeyDefaultEnvironment:  "DEFAULT_ENVIRONMENT",
+		configKeyLabelKeyPrefix:      "LABEL_KEY_PREFIX",
+		configKeyFinalizerSuffix:     "FINALIZER_SUFFIX",
+		configKeyJitterFactor:        "JITTER_FACTOR",
+		configKeyMaxBackoff:          "MAX_BACKOFF",
+		configKeyMaxConcurrentRec:    "MAX_CONCURRENT_RECONCILES",
+	}
+
+	for key, env := range envKeyMap {
+		if err := viper.BindEnv(key, env); err != nil {
+			panic(err)
+		}
+	}
 }
 
-func registerEnvs() error {
-	err := viper.BindEnv(configKeyRequeueAfterOnError, "REQUEUE_AFTER_ON_ERROR")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyRequeueAfter, "REQUEUE_AFTER")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyDefaultNamespace, "DEFAULT_NAMESPACE")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyDefaultEnvironment, "DEFAULT_ENVIRONMENT")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyLabelKeyPrefix, "LABEL_KEY_PREFIX")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyFinalizerSuffix, "FINALIZER_SUFFIX")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyJitterFactor, "JITTER_FACTOR")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyMaxBackoff, "MAX_BACKOFF")
-	if err != nil {
-		return err
-	}
-	err = viper.BindEnv(configKeyMaxConcurrentRec, "MAX_CONCURRENT_RECONCILES")
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func Parse() error {
-	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-	flag.Parse()
-	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
-		return err
-	}
-
-	if err := loadConfigFileFromFlag(); err != nil {
-		return err
-	}
-
+func Parse() {
 	RequeueAfterOnError = viper.GetDuration(configKeyRequeueAfterOnError)
 	RequeueAfter = viper.GetDuration(configKeyRequeueAfter)
 	DefaultNamespace = viper.GetString(configKeyDefaultNamespace)
 	DefaultEnvironment = viper.GetString(configKeyDefaultEnvironment)
-	LabelKeyPrefix = viper.GetString(configKeyLabelKeyPrefix)
-	FinalizerSuffix = viper.GetString(configKeyFinalizerSuffix)
-	FinalizerName = LabelKeyPrefix + "/" + FinalizerSuffix
+
 	JitterFactor = viper.GetFloat64(configKeyJitterFactor)
 	MaxBackoff = viper.GetDuration(configKeyMaxBackoff)
 	MaxConcurrentReconciles = viper.GetInt(configKeyMaxConcurrentRec)
+	LabelKeyPrefix = viper.GetString(configKeyLabelKeyPrefix)
 
-	return nil
-}
-
-func loadConfigFileFromFlag() error {
-	// Get config file path from command line flag
-	configPath := viper.GetString(configKeyFile)
-	if configPath != "" {
-		viper.SetConfigFile(configPath)
-	}
-	err := viper.ReadInConfig()
-	var configFileNotFoundError viper.ConfigFileNotFoundError
-	if err != nil && !errors.As(err, &configFileNotFoundError) {
-		return err
-	}
-	return nil
+	FinalizerName = LabelKeyPrefix + "/" + FinalizerSuffix
 }
---
+++ common/pkg/config/env_test.go
@@ -0,0 +1,178 @@
+// Copyright 2025 Deutsche Telekom IT GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package config
+
+import (
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/spf13/viper"
+)
+
+var _ = Describe("Config Env Tests", func() {
+	// Backup original values for restoration after tests
+	var (
+		origRequeueAfterOnError     time.Duration
+		origRequeueAfter            time.Duration
+		origDefaultNamespace        string
+		origDefaultEnvironment      string
+		origLabelKeyPrefix          string
+		origJitterFactor            float64
+		origMaxBackoff              time.Duration
+		origMaxConcurrentReconciles int
+		origFinalizerName           string
+
+		oldEnv map[string]string
+	)
+
+	BeforeEach(func() {
+		// Store original values
+		origRequeueAfterOnError = RequeueAfterOnError
+		origRequeueAfter = RequeueAfter
+		origDefaultNamespace = DefaultNamespace
+		origDefaultEnvironment = DefaultEnvironment
+		origLabelKeyPrefix = LabelKeyPrefix
+		origJitterFactor = JitterFactor
+		origMaxBackoff = MaxBackoff
+		origMaxConcurrentReconciles = MaxConcurrentReconciles
+		origFinalizerName = FinalizerName
+
+		// Reset viper for each test
+		viper.Reset()
+		registerDefaults()
+		oldEnv = map[string]string{}
+	})
+
+	AfterEach(func() {
+		// Restore original values
+		RequeueAfterOnError = origRequeueAfterOnError
+		RequeueAfter = origRequeueAfter
+		DefaultNamespace = origDefaultNamespace
+		DefaultEnvironment = origDefaultEnvironment
+		LabelKeyPrefix = origLabelKeyPrefix
+		JitterFactor = origJitterFactor
+		MaxBackoff = origMaxBackoff
+		MaxConcurrentReconciles = origMaxConcurrentReconciles
+		FinalizerName = origFinalizerName
+		viper.Reset()
+	})
+
+	initEnv := func(envVars map[string]string) {
+		// Clear oldEnv map first to prevent re-using old values
+		oldEnv = map[string]string{}
+
+		// Save current environment variables
+		for key := range envVars {
+			origVal, exists := os.LookupEnv(key)
+			if exists {
+				oldEnv[key] = origVal
+			}
+		}
+
+		// Set new environment variables
+		for key, value := range envVars {
+			err := os.Setenv(key, value)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		registerEnvsOrDie()
+		Parse()
+	}
+
+	cleanUpEnv := func(envVars map[string]string) {
+		for key := range envVars {
+			// If it existed before, restore it
+			if origVal, exists := oldEnv[key]; exists {
+				err := os.Setenv(key, origVal)
+				Expect(err).NotTo(HaveOccurred())
+			} else {
+				// Otherwise, unset it
+				err := os.Unsetenv(key)
+				Expect(err).NotTo(HaveOccurred())
+			}
+		}
+	}
+
+	Context("Default Values with Explicit Registration", func() {
+		It("should use default values when env not provided", func() {
+			// Ensure flags are registered but no values passed
+			registerEnvsOrDie()
+			Parse()
+
+			// Verify all values match defaults
+			// Expect(RequeueAfterOnError).To(Equal(origRequeueAfterOnError))
+			Expect(RequeueAfter).To(Equal(origRequeueAfter))
+			Expect(DefaultNamespace).To(Equal(origDefaultNamespace))
+			Expect(DefaultEnvironment).To(Equal(origDefaultEnvironment))
+			Expect(LabelKeyPrefix).To(Equal(origLabelKeyPrefix))
+			Expect(JitterFactor).To(Equal(origJitterFactor))
+			Expect(MaxBackoff).To(Equal(origMaxBackoff))
+			Expect(MaxConcurrentReconciles).To(Equal(origMaxConcurrentReconciles))
+			Expect(FinalizerName).To(Equal(origFinalizerName))
+		})
+
+		It("should load values from environment variables", func() {
+			// Set up environment variables
+			envVars := map[string]string{
+				"REQUEUE_AFTER_ON_ERROR":    "4s",
+				"REQUEUE_AFTER":             "25m",
+				"DEFAULT_NAMESPACE":         "test-namespace-from-env",
+				"DEFAULT_ENVIRONMENT":       "test-env-from-env",
+				"LABEL_KEY_PREFIX":          "test.prefix.from.env",
+				"FINALIZER_SUFFIX":          "test-finalizer-from-env",
+				"JITTER_FACTOR":             "0.6",
+				"MAX_BACKOFF":               "4m",
+				"MAX_CONCURRENT_RECONCILES": "7",
+			}
+
+			// Initialize with environment variables
+			initEnv(envVars)
+
+			// Verify environment variable values were applied
+			Expect(RequeueAfterOnError).To(Equal(4 * time.Second))
+			Expect(RequeueAfter).To(Equal(25 * time.Minute))
+			Expect(DefaultNamespace).To(Equal("test-namespace-from-env"))
+			Expect(DefaultEnvironment).To(Equal("test-env-from-env"))
+			Expect(LabelKeyPrefix).To(Equal("test.prefix.from.env"))
+			Expect(JitterFactor).To(Equal(0.6))
+			Expect(MaxBackoff).To(Equal(4 * time.Minute))
+			Expect(MaxConcurrentReconciles).To(Equal(7))
+
+			// Verify derived values
+			Expect(FinalizerName).To(Equal("test.prefix.from.env/finalizer"))
+			cleanUpEnv(envVars)
+		})
+
+		It("should override default values with environment variables", func() {
+			// Set default values different from environment variables
+			RequeueAfterOnError = 10 * time.Second
+			DefaultNamespace = "original-namespace"
+
+			// Set up environment variables
+			envVars := map[string]string{
+				"REQUEUE_AFTER_ON_ERROR": "6s",
+				"DEFAULT_NAMESPACE":      "override-namespace-from-env",
+			}
+
+			// Initialize with environment variables
+			initEnv(envVars)
+
+			// Verify environment variable values override default values
+			Expect(RequeueAfterOnError).To(Equal(6 * time.Second))
+			Expect(DefaultNamespace).To(Equal("override-namespace-from-env"))
+
+			// Verify other defaults remain unchanged
+			Expect(DefaultEnvironment).To(Equal(origDefaultEnvironment))
+			Expect(LabelKeyPrefix).To(Equal(origLabelKeyPrefix))
+			Expect(JitterFactor).To(Equal(origJitterFactor))
+			Expect(MaxBackoff).To(Equal(origMaxBackoff))
+			Expect(MaxConcurrentReconciles).To(Equal(origMaxConcurrentReconciles))
+			Expect(FinalizerName).To(Equal(LabelKeyPrefix + "/" + FinalizerSuffix))
+			cleanUpEnv(envVars)
+		})
+	})
+})
