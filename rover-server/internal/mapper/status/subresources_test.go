9cc706fc97ac4d7f4df4429adefdc794531caeae
Ron Gummich <ron.gummich@telekom.de>
2025-08-01 11:26:12 +0200

    test(rover-server): use ginkgo for tests

--- rover-server/internal/mapper/status/subresources_test.go
+++ rover-server/internal/mapper/status/subresources_test.go
@@ -6,45 +6,53 @@ package status
 
 import (
 	"context"
-	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	apiv1 "github.com/telekom/controlplane/api/api/v1"
 	v1 "github.com/telekom/controlplane/rover/api/v1"
 )
 
-func TestGetAllSubResources_NilRover(t *testing.T) {
-	// given
-	ctx := context.Background()
-	mockStore := new(MockObjectStore[*apiv1.ApiSubscription])
-	var rover *v1.Rover = nil
+var _ = Describe("GetAllSubResources", func() {
+	Context("when rover is nil", func() {
+		It("returns an error", func() {
+			// given
+			ctx := context.Background()
+			mockStore := new(MockObjectStore[*apiv1.ApiSubscription])
+			var rover *v1.Rover = nil
 
-	// when
-	result, err := getAllSubResources(ctx, rover, mockStore)
+			// when
+			result, err := getAllSubResources(ctx, rover, mockStore)
 
-	// then
-	assert.Error(t, err)
-	assert.Nil(t, result)
-	assert.Contains(t, err.Error(), "rover resource is not processed and does not contain an application")
-	mockStore.AssertExpectations(t)
-}
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(result).To(BeNil())
+			Expect(err.Error()).To(ContainSubstring("rover resource is not processed and does not contain an application"))
+			// Note: mock assertions are still using testify style since we need the mock object
+			mockStore.AssertExpectations(GinkgoT())
+		})
+	})
 
-func TestGetAllSubResources_NilApplication(t *testing.T) {
-	// given
-	ctx := context.Background()
-	mockStore := new(MockObjectStore[*apiv1.ApiSubscription])
-	rover := &v1.Rover{
-		Status: v1.RoverStatus{
-			// Application is nil
-		},
-	}
+	Context("when rover application is nil", func() {
+		It("returns an error", func() {
+			// given
+			ctx := context.Background()
+			mockStore := new(MockObjectStore[*apiv1.ApiSubscription])
+			rover := &v1.Rover{
+				Status: v1.RoverStatus{
+					// Application is nil
+				},
+			}
 
-	// when
-	result, err := getAllSubResources(ctx, rover, mockStore)
+			// when
+			result, err := getAllSubResources(ctx, rover, mockStore)
 
-	// then
-	assert.Error(t, err)
-	assert.Nil(t, result)
-	assert.Contains(t, err.Error(), "rover resource is not processed and does not contain an application")
-	mockStore.AssertExpectations(t)
-}
+			// then
+			Expect(err).To(HaveOccurred())
+			Expect(result).To(BeNil())
+			Expect(err.Error()).To(ContainSubstring("rover resource is not processed and does not contain an application"))
+			// Note: mock assertions are still using testify style since we need the mock object
+			mockStore.AssertExpectations(GinkgoT())
+		})
+	})
+})
