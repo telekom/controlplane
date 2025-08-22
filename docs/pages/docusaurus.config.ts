d54412c91de8aae0a8e11c682541bc9e924500a6
Bjoern Kottner <1780890+BjoernKarma@users.noreply.github.com>
2025-08-22 16:48:21 +0200

    feat(docs): Update docusaurus with defaults from new site

--- docs/pages/docusaurus.config.ts
+++ docs/pages/docusaurus.config.ts
@@ -4,6 +4,8 @@ import rehypeKatex from 'rehype-katex';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
 const config: Config = {
   title: 'Controlplane Technical Documentation',
   tagline: 'Technical frameworks and architecture for the Open Telekom Integration Platform',
@@ -17,14 +19,20 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'telekom', // Usually your GitHub org/user name.
+  projectName: 'controlplane', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -81,7 +89,7 @@ const config: Config = {
           position: 'left',
         },
         {
-          href: 'https://github.com/yourusername/controlplane',
+          href: 'https://github.com/telekom/controlplane',
           label: 'GitHub',
           position: 'right',
         },
@@ -117,7 +125,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/yourusername/controlplane',
+              href: 'https://github.com/telekom/controlplane',
             },
           ],
         },
