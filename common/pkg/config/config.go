98baeb652a4f2f72beb468925791d4ab00c79670
Ron Gummich <ron.gummich@telekom.de>
2025-10-06 16:22:20 +0200

    fix(config): added controller-startup-window key

--- common/pkg/config/config.go
+++ common/pkg/config/config.go
@@ -21,6 +21,7 @@ const (
 	configKeyJitterFactor        = "jitter-factor"
 	configKeyMaxBackoff          = "max-backoff"
 	configKeyMaxConcurrentRec    = "max-concurrent-reconciles"
+	configKeyCtrlStartupWindow   = "controller-startup-window"
 )
 
 const (
@@ -62,6 +63,7 @@ func registerDefaults() {
 	viper.SetDefault(configKeyJitterFactor, JitterFactor)
 	viper.SetDefault(configKeyMaxBackoff, MaxBackoff)
 	viper.SetDefault(configKeyMaxConcurrentRec, MaxConcurrentReconciles)
+	viper.SetDefault(configKeyCtrlStartupWindow, 0)
 }
 
 func registerEnvs() {
