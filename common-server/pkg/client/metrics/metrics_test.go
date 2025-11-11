d80856000d3268e2b4f72f87410eb63391cc84ff
stefan-ctrl <stefan-ctrl@users.noreply.github.com>
2025-11-11 13:14:32 +0100

    feat(common-server): add additional metrics for cc-client in case of no response from server

--- common-server/pkg/client/metrics/metrics_test.go
+++ common-server/pkg/client/metrics/metrics_test.go
@@ -5,14 +5,19 @@
 package metrics_test
 
 import (
+	"context"
+	"crypto/tls"
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"regexp"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/prometheus/client_golang/prometheus"
+	prometheusdto "github.com/prometheus/client_model/go"
 	client "github.com/telekom/controlplane/common-server/pkg/client/metrics"
 )
 
@@ -30,7 +35,7 @@ var _ = Describe("Client Metrics", func() {
 	Context("Collecting Metrics", func() {
 		ExpectedMetricName := "http_client_request_duration_seconds"
 
-		It("should collect metrics when asuccessful request is made", func() {
+		It("should collect metrics when successful request is made", func() {
 			httpClient := &mockClient{}
 
 			httpDoer := client.WithMetrics(httpClient, client.WithClientName("testClient"))
@@ -46,77 +51,28 @@ var _ = Describe("Client Metrics", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(metrics).ToNot(BeEmpty())
 
-			var foundMetric bool
+			m := findMetric(metrics, ExpectedMetricName, "testClient")
+			Expect(m).ToNot(BeNil())
+			Expect(m.GetLabel()[0].GetName()).To(Equal("client"))
+			Expect(m.GetLabel()[0].GetValue()).To(Equal("testClient"))
+
+			Expect(m.GetLabel()[1].GetName()).To(Equal("method"))
+			Expect(m.GetLabel()[1].GetValue()).To(Equal("GET"))
+
+			Expect(m.GetLabel()[2].GetName()).To(Equal("path"))
+			Expect(m.GetLabel()[2].GetValue()).To(Equal("/test/path"))
+
+			Expect(m.GetLabel()[3].GetName()).To(Equal("status"))
+			Expect(m.GetLabel()[3].GetValue()).To(Equal("200"))
+			Expect(m.GetHistogram().GetSampleCount()).To(BeNumerically(">", 0))
+
+			foundMetric := false
 			for _, m := range metrics {
 				if m.GetName() == ExpectedMetricName {
 					foundMetric = true
 
 					Expect(m.GetType().String()).To(Equal("HISTOGRAM"))
 					Expect(m.GetHelp()).To(Equal("Duration of HTTP requests in seconds"))
-
-					Expect(m.GetMetric()).To(HaveLen(1))
-					metric := m.GetMetric()[0]
-					Expect(metric.GetLabel()).To(HaveLen(4))
-					Expect(metric.GetLabel()[0].GetName()).To(Equal("client"))
-					Expect(metric.GetLabel()[0].GetValue()).To(Equal("testClient"))
-
-					Expect(metric.GetLabel()[1].GetName()).To(Equal("method"))
-					Expect(metric.GetLabel()[1].GetValue()).To(Equal("GET"))
-
-					Expect(metric.GetLabel()[2].GetName()).To(Equal("path"))
-					Expect(metric.GetLabel()[2].GetValue()).To(Equal("/test/path"))
-
-					Expect(metric.GetLabel()[3].GetName()).To(Equal("status"))
-					Expect(metric.GetLabel()[3].GetValue()).To(Equal("200"))
-
-					Expect(metric.GetHistogram().GetSampleCount()).To(BeNumerically(">", 0))
-				}
-			}
-			Expect(foundMetric).To(BeTrue(), "Expected metric %s not found", ExpectedMetricName)
-		})
-
-		It("should collect metrics when a failed request is made", func() {
-			httpClient := &mockClient{
-				do: func(req *http.Request) (*http.Response, error) {
-					return nil, fmt.Errorf("error")
-				},
-			}
-
-			httpDoer := client.WithMetrics(httpClient, client.WithClientName("testClient"))
-			Expect(httpDoer).ToNot(BeNil())
-
-			req := httptest.NewRequest("GET", "/test/path", nil)
-			res, err := httpDoer.Do(req)
-			Expect(err).To(HaveOccurred())
-			Expect(res).To(BeNil())
-
-			metrics, err := prometheus.DefaultGatherer.Gather()
-			Expect(err).ToNot(HaveOccurred())
-			Expect(metrics).ToNot(BeEmpty())
-
-			var foundMetric bool
-			for _, m := range metrics {
-				if m.GetName() == ExpectedMetricName {
-					foundMetric = true
-
-					Expect(m.GetType().String()).To(Equal("HISTOGRAM"))
-					Expect(m.GetHelp()).To(Equal("Duration of HTTP requests in seconds"))
-
-					Expect(m.GetMetric()).To(HaveLen(2))
-					metric := m.GetMetric()[1]
-					Expect(metric.GetLabel()[0].GetName()).To(Equal("client"))
-					Expect(metric.GetLabel()[0].GetValue()).To(Equal("testClient"))
-
-					Expect(metric.GetLabel()[1].GetName()).To(Equal("method"))
-					Expect(metric.GetLabel()[1].GetValue()).To(Equal("GET"))
-
-					Expect(metric.GetLabel()[2].GetName()).To(Equal("path"))
-					Expect(metric.GetLabel()[2].GetValue()).To(Equal("/test/path"))
-
-					Expect(metric.GetLabel()[3].GetName()).To(Equal("status"))
-					Expect(metric.GetLabel()[3].GetValue()).To(Equal("error"))
-
-					Expect(metric.GetHistogram().GetSampleCount()).To(BeNumerically(">", 0))
 				}
 			}
 			Expect(foundMetric).To(BeTrue(), "Expected metric %s not found", ExpectedMetricName)
@@ -174,3 +130,140 @@ var _ = Describe("ReplacePath", func() {
 	})
 
 })
+
+var _ = Describe("Error Categorization", func() {
+	var errorReturn error
+	httpClient := &mockClient{
+		do: func(req *http.Request) (*http.Response, error) {
+			return nil, errorReturn
+		},
+	}
+
+	testErrorCategorization := func(clientName string, expectedStatus string) {
+		httpDoer := client.WithMetrics(httpClient, client.WithClientName(clientName))
+		req := httptest.NewRequest("GET", "/test", nil)
+		_, err := httpDoer.Do(req)
+
+		if errorReturn != nil {
+			Expect(err).To(HaveOccurred())
+		}
+
+		metrics, err := prometheus.DefaultGatherer.Gather()
+		Expect(err).ToNot(HaveOccurred())
+
+		status := findMetricStatus(metrics, "http_client_request_duration_seconds", clientName)
+		Expect(status).To(Equal(expectedStatus))
+	}
+
+	Context("Timeout Errors", func() {
+		It("should categorize context.DeadlineExceeded as error:timeout", func() {
+			errorReturn = context.DeadlineExceeded
+			testErrorCategorization("timeout-test", "error:timeout")
+		})
+
+		It("should categorize os.ErrDeadlineExceeded as error:timeout", func() {
+			errorReturn = os.ErrDeadlineExceeded
+			testErrorCategorization("timeout-test2", "error:timeout")
+		})
+
+		It("should categorize net.Error timeout as error:timeout", func() {
+			errorReturn = &timeoutError{}
+			testErrorCategorization("timeout-test3", "error:timeout")
+		})
+	})
+
+	Context("Canceled Requests", func() {
+		It("should categorize context.Canceled as error:canceled", func() {
+			errorReturn = context.Canceled
+			testErrorCategorization("canceled-test", "error:canceled")
+		})
+	})
+
+	Context("TLS Errors", func() {
+		It("should categorize tls.RecordHeaderError as error:tls", func() {
+			errorReturn = tls.RecordHeaderError{Msg: "bad record MAC"}
+			testErrorCategorization("tls-test", "error:tls")
+		})
+
+		It("should categorize errors containing 'tls' as error:tls", func() {
+			errorReturn = fmt.Errorf("tls handshake failed")
+			testErrorCategorization("tls-test2", "error:tls")
+		})
+
+		It("should categorize errors containing 'certificate' as error:tls", func() {
+			errorReturn = fmt.Errorf("certificate verify failed")
+			testErrorCategorization("tls-test3", "error:tls")
+		})
+	})
+
+	Context("Connection Refused Errors", func() {
+		It("should categorize dial OpError as error:connection_refused", func() {
+			errorReturn = &net.OpError{Op: "dial", Err: fmt.Errorf("connection refused")}
+			testErrorCategorization("conn-refused-test", "error:connection_refused")
+		})
+
+		It("should categorize errors containing 'connection refused' as error:connection_refused", func() {
+			errorReturn = fmt.Errorf("dial tcp: connection refused")
+			testErrorCategorization("conn-refused-test2", "error:connection_refused")
+		})
+	})
+
+	Context("DNS Errors", func() {
+		It("should categorize net.DNSError as error:dns", func() {
+			errorReturn = &net.DNSError{}
+			testErrorCategorization("dns-test", "error:dns")
+		})
+	})
+
+	Context("Generic Errors", func() {
+		It("should categorize unknown errors as error", func() {
+			errorReturn = fmt.Errorf("some unknown error")
+			testErrorCategorization("generic-test", "error")
+		})
+	})
+
+	Context("No Response", func() {
+		It("should categorize nil error with nil response as error:no_response", func() {
+			errorReturn = nil
+			testErrorCategorization("no-response-test", "error:no_response")
+		})
+	})
+})
+
+func findMetric(metrics []*prometheusdto.MetricFamily, metricName, clientName string) *prometheusdto.Metric {
+	for _, m := range metrics {
+		if m.GetName() == metricName {
+			for _, metric := range m.GetMetric() {
+				labels := metric.GetLabel()
+
+				for _, label := range labels {
+					if label.GetName() == "client" && label.GetValue() == clientName {
+						return metric
+					}
+				}
+
+			}
+		}
+	}
+	return nil
+}
+
+// Helper function to find the status label value for a specific client
+func findMetricStatus(metrics []*prometheusdto.MetricFamily, metricName, clientName string) string {
+	metric := findMetric(metrics, metricName, clientName)
+	if metric != nil {
+		for _, label := range metric.GetLabel() {
+			if label.GetName() == "status" {
+				return label.GetValue()
+			}
+		}
+	}
+	return ""
+}
+
+// timeoutError implements net.Error with Timeout() returning true
+type timeoutError struct{}
+
+func (e *timeoutError) Error() string   { return "timeout error" }
+func (e *timeoutError) Timeout() bool   { return true }
+func (e *timeoutError) Temporary() bool { return true }
