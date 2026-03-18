d774892206eb4c45b9752fe52b81ae472850c6ce
stefan-ctrl <stefan-ctrl@users.noreply.github.com>
2026-03-18 15:28:32 +0100

    test(controlplane-api): verify OwnerTeam resolver

--- controlplane-api/internal/resolvers/resolver.go
+++ controlplane-api/internal/resolvers/resolver.go
@@ -12,6 +12,11 @@ import (
 // Resolver is the root resolver for the GraphQL API.
 type Resolver struct{ client *ent.Client }
 
+// NewResolver creates a new root resolver with the given ent client.
+func NewResolver(client *ent.Client) *Resolver {
+	return &Resolver{client: client}
+}
+
 // NewSchema creates a graphql executable schema.
 func NewSchema(client *ent.Client) graphql.ExecutableSchema {
 	return NewExecutableSchema(Config{
