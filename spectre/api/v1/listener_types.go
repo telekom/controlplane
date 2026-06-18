006ded47a17a544c157a5547dcd8b1d02c80e27d
Ivan Krylov <ivan.krylov@telekom.de>
2026-06-18 18:38:31 +0200

    chore(spectre): scaffold kubebuilder structure with listener kind

---
+++ spectre/api/v1/listener_types.go
@@ -0,0 +1,85 @@
+// SPDX-FileCopyrightText: 2026 Deutsche Telekom IT GmbH
+// Copyright 2026.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ListenerSpec defines the desired state of Listener
+type ListenerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// The following markers will use OpenAPI v3 schema to validate the value
+	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
+
+	// foo is an example field of Listener. Edit listener_types.go to remove/update
+	// +optional
+	Foo *string `json:"foo,omitempty"`
+}
+
+// ListenerStatus defines the observed state of Listener.
+type ListenerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// For Kubernetes API conventions, see:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	// conditions represent the current state of the Listener resource.
+	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
+	//
+	// Standard condition types include:
+	// - "Available": the resource is fully functional
+	// - "Progressing": the resource is being created or updated
+	// - "Degraded": the resource failed to reach or maintain its desired state
+	//
+	// The status of each condition is one of True, False, or Unknown.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Listener is the Schema for the listeners API
+type Listener struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of Listener
+	// +required
+	Spec ListenerSpec `json:"spec"`
+
+	// status defines the observed state of Listener
+	// +optional
+	Status ListenerStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// ListenerList contains a list of Listener
+type ListenerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []Listener `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(func(s *runtime.Scheme) error {
+		s.AddKnownTypes(SchemeGroupVersion, &Listener{}, &ListenerList{})
+		return nil
+	})
+}
