c7a9ccf4367f9f0be6f4ddf67fa6286f7fd49173
Ivan Krylov <ivan.krylov@telekom.de>
2026-06-19 13:46:53 +0200

    chore(spectre): add comments regarding handler error handling and condition system

--- spectre/internal/handler/listener_handler.go
+++ spectre/internal/handler/listener_handler.go
@@ -14,10 +14,12 @@ type ListenerHandler struct{}
 
 func (ls *ListenerHandler) CreateOrUpdate(ctx context.Context, listener *spectrev1.Listener) error {
 	// todo: add actual logic
+	// see predefined errors here: https://telekom.github.io/controlplane/docs/developer-journey/creating-an-operator#error-handling
 	return nil
 }
 
 func (ls *ListenerHandler) Delete(ctx context.Context, listener *spectrev1.Listener) error {
 	// todo: add actual logic, if needed
+	// see predefined errors here: https://telekom.github.io/controlplane/docs/developer-journey/creating-an-operator#error-handling
 	return nil
 }
