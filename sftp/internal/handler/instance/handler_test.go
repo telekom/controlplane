df236ebe762be7c9e11dec5e46edd0e2af5c540b
Lukas Koszegy <lukas.koszegy@telekom.com>
2026-08-19 13:52:29 +0200

    feat(sftp): optimize error handling (#546)

--- sftp/internal/handler/instance/handler_test.go
+++ sftp/internal/handler/instance/handler_test.go
@@ -7,6 +7,7 @@ package instance
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/stretchr/testify/mock"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -62,13 +63,12 @@ var _ = Describe("InstanceHandler", func() {
 
 	It("retries when the referenced SFTPServiceConfig service is unavailable", func() {
 		handler, ctx, instance, _ := newTestHandlerWithFactory(recordingFactory{
-			err: ctrlerrors.RetryableErrorf("SFTP client for SFTPServiceConfig %q is not initialized", "test/test-sftpServiceConfig"),
+			err: fmt.Errorf("SFTP client for SFTPServiceConfig %q is not initialized", "test/test-sftpServiceConfig"),
 		})
 
 		err := handler.CreateOrUpdate(ctx, instance)
 
-		var retryable ctrlerrors.RetryableError
-		Expect(errors.As(err, &retryable)).To(BeTrue())
+		Expect(err).To(HaveOccurred())
 		Expect(err).To(MatchError(ContainSubstring("SFTPServiceConfig")))
 		Expect(err).To(MatchError(ContainSubstring("not initialized")))
 	})
@@ -137,6 +137,13 @@ var _ = Describe("InstanceHandler", func() {
 		err := handler.Delete(ctx, instance)
 
 		Expect(err).To(MatchError("service unavailable"))
+	})
+
+	It("does not fail delete when SFTP user does not exist in external service", func() {
+		handler, ctx, instance, mockService := newTestHandler()
+		expectDeleteSFTPUser(mockService, service.ErrNotFound)
+
+		Expect(handler.Delete(ctx, instance)).To(Succeed())
 	})
 
 	It("wraps service user deletion errors", func() {
