85a332d9b4feec28a556925df02976743c269cc7
peter.puha <peter.puha@telekom.com>
2026-09-03 08:01:33 +0200

    temove zone check from filesubscription

--- projector/internal/domain/filesubscription/translator.go
+++ projector/internal/domain/filesubscription/translator.go
@@ -39,11 +39,16 @@ func (t *Translator) Translate(_ context.Context, obj *filev1.FileSubscription) 
 		}
 	}
 
+	var zoneName string
+	if obj.Spec.Zone != nil {
+		zoneName = obj.Spec.Zone.Name
+	}
+
 	return &FileSubscriptionData{
 		Meta:           shared.NewMetadata(obj.Namespace, obj.Name, obj.Labels),
 		StatusPhase:    phase,
 		StatusMessage:  message,
-		Zone:           obj.Spec.Zone.Name,
+		Zone:           zoneName,
 		SFTPPublicKeys: publicKeys,
 		OwnerAppName:   obj.Labels[applicationLabelKey],
 		OwnerTeamName:  shared.TeamNameFromNamespace(obj.Namespace),
