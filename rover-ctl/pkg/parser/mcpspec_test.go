0cb6605bb1849531b4605c77006cb1c8e1a474ec
Luis Pflamminger <luis.pflamminger@telekom.de>
2026-09-23 16:42:26 +0200

    test(rover-ctl): cover YAML anchors in specifications

--- rover-ctl/pkg/parser/mcpspec_test.go
+++ rover-ctl/pkg/parser/mcpspec_test.go
@@ -5,6 +5,7 @@
 package parser_test
 
 import (
+	"encoding/json"
 	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -52,6 +53,32 @@ var _ = Describe("McpSpecification Parsing", func() {
 			Expect(objects[0].GetKind()).To(Equal("McpSpecification"))
 			Expect(objects[0].GetApiVersion()).To(Equal("tcp.ei.telekom.de/v1"))
 			Expect(objects[0].GetName()).To(Equal("team-assistant"))
+		})
+
+		It("should expand anchors and merge keys", func() {
+			mcpParser := parser.NewObjectParser(parser.Opts...)
+			Expect(mcpParser.Parse(filepath.Join("testdata", "mcp-spec-with-anchors.yaml"))).To(Succeed())
+
+			objects := mcpParser.Objects()
+			Expect(objects).To(HaveLen(1))
+			Expect(objects[0].GetContent()["tools"]).To(ConsistOf(
+				And(
+					HaveKeyWithValue("name", "summarize"),
+					HaveKeyWithValue("description", "Process the provided text"),
+					HaveKeyWithValue("inputSchema", HaveKeyWithValue("type", "object")),
+				),
+				And(
+					HaveKeyWithValue("name", "translate"),
+					HaveKeyWithValue("inputSchema", HaveKeyWithValue("type", "object")),
+				),
+			))
+
+			parsedJSON, err := json.Marshal(objects[0].GetContent())
+			Expect(err).NotTo(HaveOccurred())
+			// Parsers that do not resolve aliases correctly can strip * but leave the anchor name as a value.
+			// Checking the name catches that case and also catches any unexpanded * or & syntax.
+			Expect(parsedJSON).NotTo(ContainSubstring("mcp_tool_defaults_anchor"))
+			Expect(parsedJSON).NotTo(ContainSubstring("mcp_text_schema_anchor"))
 		})
 	})
 })
